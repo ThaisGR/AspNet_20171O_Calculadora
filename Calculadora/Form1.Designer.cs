8f939947b7db0cb833c49be6897c87c23fdb4944
6458 <6458@034LAB04044>
2017-02-24 21:27:34 -0300

    Desabilitado o campo do display

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -34,6 +34,7 @@
             // 
             // txtDisplay
             // 
+            this.txtDisplay.Enabled = false;
             this.txtDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtDisplay.Location = new System.Drawing.Point(12, 12);
             this.txtDisplay.Name = "txtDisplay";
